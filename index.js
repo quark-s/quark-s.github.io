e32f96988625e5da6bfa62c01d3d22520ea39440
Felix Wagner <wagnerf@dipf.de>
2022-01-26 14:17:36 +0100

    player / condition eventhandlers

--- index.js
+++ index.js
@@ -43,59 +43,73 @@ TStage.loadTrackData(trackData);
 
     document.getElementById("bRotateRight").onclick = function(e) {rotate(1,this);}
     document.getElementById("bRotateLeft").onclick = function(e) {rotate(0,this);}
-    document.getElementById("bDelete").onclick = function(e) {if(!!TStage.getSelectedTrack()) TStage.removeTrack(TStage.getSelectedTrack());}
+    if(!!document.getElementById("bDelete")){
+		document.getElementById("bDelete").onclick = function(e) {if(!!TStage.getSelectedTrack()) TStage.removeTrack(TStage.getSelectedTrack());}
+	}
+	
+	if(!!document.getElementById("bSaveStage")){
+		document.getElementById("bSaveStage").onclick = function(e) {
+			TStage.saveCurrentStage();
+			document.getElementById("bRestoreStage").removeAttribute("disabled");
+			console.log(TStage.getSavedTrackData());
+		}
+	}
 
-    document.getElementById("bSaveStage").onclick = function(e) {
-        TStage.saveCurrentStage();
-        document.getElementById("bRestoreStage").removeAttribute("disabled");
-        console.log(TStage.getSavedTrackData());
-    }
+	if(!!document.getElementById("bRestoreStage")){
+		document.getElementById("bRestoreStage").onclick = function(e) {
+			TStage.restoreTrackData();
+			document.getElementById("bForward").setAttribute("disabled", 1);
+			document.getElementById("bBack").setAttribute("disabled", 1);
+		}
+	}
 
-    document.getElementById("bRestoreStage").onclick = function(e) {
-        TStage.restoreTrackData();
-        document.getElementById("bForward").setAttribute("disabled", 1);
-        document.getElementById("bBack").setAttribute("disabled", 1);
-    }
+	
+	if(!!document.getElementById("bBack")){
+		document.getElementById("bBack").onclick = function(e) {        
+			if(TStage.undo())
+				document.getElementById("bForward").removeAttribute("disabled");
+			if(TStage.getCurrentIndex() == 0)
+				document.getElementById("bBack").setAttribute("disabled", 1);        
+		};
+	}
 
-    document.getElementById("bBack").onclick = function(e) {        
-        if(TStage.undo())
-            document.getElementById("bForward").removeAttribute("disabled");
-        if(TStage.getCurrentIndex() == 0)
-            document.getElementById("bBack").setAttribute("disabled", 1);        
-    };
-
-    document.getElementById("bForward").onclick = function(e) {
-        if(TStage.redo()){
-            document.getElementById("bBack").removeAttribute("disabled");
-        }
-        if(TStage.getCurrentIndex() == TStage.getStageDataHistory().length-1 )
-            document.getElementById("bForward").setAttribute("disabled", 1);
-    };
-
+	if(!!document.getElementById("bForward")){
+		document.getElementById("bForward").onclick = function(e) {
+			if(TStage.redo()){
+				document.getElementById("bBack").removeAttribute("disabled");
+			}
+			if(TStage.getCurrentIndex() == TStage.getStageDataHistory().length-1 )
+				document.getElementById("bForward").setAttribute("disabled", 1);
+		};
+	}
 
 
-    let trackSel = document.getElementById("addTrackType");
-    trackTypes.forEach(t => {
-        var option = document.createElement("option");
-        option.value = t;
-        option.text = t;
-        trackSel.appendChild(option);
-    });
+	if(!!document.getElementById("addTrackType")){
+		let trackSel = document.getElementById("addTrackType");
+		trackTypes.forEach(t => {
+			var option = document.createElement("option");
+			option.value = t;
+			option.text = t;
+			trackSel.appendChild(option);
+		});
+	}
 
-    document.getElementById("bAddTrack").onclick = function(e) {
-        var button = this;
-        button.setAttribute("disabled", 1);
-        TStage.addTrack(
-            {           
-                type: document.getElementById("addTrackType").value || "TrackType1",
-                pos: {x:50, y:50},
-                width: parseInt(document.getElementById("addTrackWidth").value) || 2,
-                height: parseInt(document.getElementById("addTrackHeight").value) || 6  
-            }         
-        );
-        window.setTimeout(e => {
-            button.removeAttribute("disabled");
-        }, 1000);
-    };
+	if(!!document.getElementById("bAddTrack")){
+		document.getElementById("bAddTrack").onclick = function(e) {
+			var button = this;
+			button.setAttribute("disabled", 1);
+			TStage.addTrack(
+				{           
+					type: document.getElementById("addTrackType").value || "TrackType1",
+					pos: {x:50, y:50},
+					width: parseInt(document.getElementById("addTrackWidth").value) || 2,
+					height: parseInt(document.getElementById("addTrackHeight").value) || 6  
+				}         
+			);
+			window.setTimeout(e => {
+				button.removeAttribute("disabled");
+			}, 1000);
+		};
+	}
 
  })();
