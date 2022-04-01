4143f89b4895222521cd70f6d9450de6473748f6
Felix Wagner <wagnerf@dipf.de>
2022-04-01 12:19:52 +0200

    interface / intro

---
+++ shapes/demo.js
@@ -0,0 +1,35 @@
+var trackData = [
+    {
+        "type": "DemoShape2",
+        "pos": {
+            "x": 170,
+            "y": 470
+        },
+        "width": 4.5,
+        "height": 10,
+        "rotation": 0,
+        "connectors": []
+    },
+    {
+        "type": "DemoShape1",
+        "pos": {
+            "x": 650,
+            "y": 470
+        },
+        "width": 5,
+        "height": 8.5,
+        "rotation": 45,
+        "connectors": []
+    },
+    {
+        "type": "DemoShape3",
+        "pos": {
+            "x": 450,
+            "y": 470
+        },
+        "width": 5,
+        "height": 10,
+        "rotation": 180,
+        "connectors": []
+    }
+]
